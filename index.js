7341594889dffb1679523f54af39335a596603c4
jomapormentilla <jomapormentilla@gmail.com>
2021-02-03 14:00:17 -0500

    commit tree 43f5220c8a69f24e60e099de7c54c7c78394e8e9

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+] 
+
+function superbowlWin( obj ) {
+  let result = obj.find(prop => {
+    return prop["result"] === "W"
+  })
+
+  if (result) {
+    return result["year"]
+  }
+}
+
+superbowlWin(record)
